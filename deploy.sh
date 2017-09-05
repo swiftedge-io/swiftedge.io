71e4ad03dcf690d52023e5db551c9a53a52d0971
Alejo Rodriguez <a.rodsott@gmail.com>
2017-09-05 10:13:13 -0300

    update build process

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+scp -o StrictHostKeyChecking=no dist.tar $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST << EOF
+  tar -xf $DEPLOY_PATH/dist.tar $DEPLOY_PATH/
+  pm2 stop $WEB_PROCESS_NAME
+  pm2 start $DEPLOY_PATH/dist/server.js --name $WEB_PROCESS_NAME --node-args="--optimize_for_size --max_old_space_size=460 --gc_interval=100"
+EOF
+rm dist.tar

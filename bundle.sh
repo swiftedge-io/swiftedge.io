448df8fc54ed4c79d3ee2c6156c0a2e3307c6d58
Alejo Rodriguez <a.rodsott@gmail.com>
2017-09-05 18:15:56 -0300

    add config to deploy

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,12 @@
+touch config.json
+echo << EOL
+{
+  "gmailUser": "GMAIL_USER"
+  "gmailPass": "GMAIL_PASS"
+}
+EOL >> config.json
 rm -rf dist && mkdir -p dist
-cp {index.html,package.json} dist/
+cp {index.html,package.json,config.json} dist/
 cp -a server dist/
 cp -a static dist/
 cd dist && npm install --production && cd ../
